6e7880ab5186a6c2d18416c0351e74a5f72a6c0c
guillerminafara <marfarsan@alu.edu.gva.es>
2024-10-10 20:22:55 +0200

    10/10

---
+++ activity_scrolling/app/src/main/java/com/example/activity_scrolling/ScrollingActivity.kt
@@ -0,0 +1,22 @@
+package com.example.activity_scrolling
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.activity_scrolling.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
