e66a86e6a9b7f88329c69bbcaf99a6d8c0fa8703
guillerminafara <marfarsan@alu.edu.gva.es>
2024-10-29 16:55:00 +0100

    29/10

--- u2/CentroComercial/app/build.gradle.kts
+++ u2/CentroComercial/app/build.gradle.kts
@@ -33,10 +33,16 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    dataBinding{
+        enable = true
+    }
+    buildFeatures{
+        viewBinding= true
+    }
 }
 
 dependencies {
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- u2/CentroComercial/app/src/main/java/com/example/centrocomercial/MainActivity.kt
+++ u2/CentroComercial/app/src/main/java/com/example/centrocomercial/MainActivity.kt
@@ -2,21 +2,35 @@ package com.example.centrocomercial
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+import com.example.centrocomercial.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     private var mplayer: MediaPlayer?=null
     private var position: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        var url: String="https://www.lovevalencia.com/wp-content/uploads/2012/01/Nuevo-centro.jpg";
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        imagen(binding.ImagenCentro1, url)
+        imagen(binding.ImagenCentro2, "https://estaticos-cdn.prensaiberica.es/clip/d86ace57-dea1-4766-b6ac-f5e3ebffd012_16-9-discover-aspect-ratio_default_0.jpg")
 
 
     }
+    fun imagen(imagen: ImageView, linnk: String){
+        Glide.with(this).load(linnk).into(imagen)
+    }
+
     override fun onStart(){
         super.onStart()
         mplayer = MediaPlayer.create(this,R.raw.vivelavida)
@@ -50,5 +64,6 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
     }
+
 
 }
